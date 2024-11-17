8227481b41bd9b974c2acd7a13cb2cc31cae46e6
nikagos <nikagos@hotmail.com>
2024-11-17 22:52:32 +0000

    updated logic of truncating/creating the table

--- ingestion_flows/main_ingest.py
+++ ingestion_flows/main_ingest.py
@@ -62,17 +62,18 @@ def ingest_into_postgres(df: pd.DataFrame, engine: Engine, table_name: str) -> N
     print(table_exists)
 
     # Check if table exists before truncating
-    if table_exists:
-        print(f"Table {table_name}. Truncating first...")
-        engine.execute(truncate_table_query)
-        df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
-        print("Data was ingested.")
-    else:
+    if not table_exists:
         print(f"Table {table_name} does not exist. Skipped truncating and creating it in the database.")
         df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace', index=False)
         print("Table created.")
         df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
         print("Data was ingested.")
+    else:
+        print(f"Table {table_name}. Truncating first...")
+        engine.execute(truncate_table_query)
+        df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
+        print("Data was ingested.")
+
 
     # # Check if the table exists
     # result = engine.execute(check_table_exists_query, {'table_name': table_name}).fetchone()
